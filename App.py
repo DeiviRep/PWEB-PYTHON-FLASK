c9dbb03a30a5a0e63cc87d7e5857f9d4c337b1c7
Deivi Patzi <patzivargasdavid@gmail.com>
2022-10-21 20:09:44 -0400

    C2 Modificacion de login

--- App.py
+++ App.py
@@ -26,7 +26,7 @@ app.secret_key= 'mysecretkey'
 @app.route('/')
 def Index():
     cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM user')
+    cur.execute('SELECT * FROM login')
     data = cur.fetchall()
     return render_template('index.html',contacts = data)#retornamos o renderisamos plantilla
 
@@ -36,10 +36,9 @@ def add_contact():
     if request.method == 'POST':
         usuario = request.form['usuario']
         password = request.form['password']
-        email = request.form['email']
         #cursor es para manejar nuestra coneccion
         cur = mysql.connection.cursor()
-        cur.execute('INSERT INTO user(usuario,email,contraseña) VALUES(%s,%s,aes_encrypt(%s,"toto"));',(usuario,email,password))
+        cur.execute('INSERT INTO login(usuario,password) VALUES(%s,aes_encrypt(%s,"toto"));',(usuario,password))
         #cur.execute('INSERT INTO user(usuario,email,contraseña) VALUES(%s,%s,%s);',(usuario,email,password))
         mysql.connection.commit()
         flash('contacto added succesfull')
@@ -49,7 +48,7 @@ def add_contact():
 @app.route('/edit/<id>')
 def get_contact(id):
     cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM user WHERE id = {0};'.format(id))
+    cur.execute('SELECT * FROM login WHERE id_l = {0};'.format(id))
     data = cur.fetchall()
     return render_template('edit.html',contact = data[0])
 
@@ -58,15 +57,13 @@ def update_contact(id):
      if request.method == 'POST':
         usuario = request.form['usuario']
         password = request.form['password']
-        email = request.form['email']
         cur = mysql.connection.cursor()
         cur.execute("""
-            UPDATE user
+            UPDATE login
             SET usuario = %s,
-                contraseña = %s,
-                email = %s
-            WHERE id = %s
-                """,(usuario,password,email,id))
+                password = %s
+            WHERE id_l = %s
+                """,(usuario,password,id))
         mysql.connection.commit()
         flash('Contact Update Successfully')
         return redirect(url_for('Index'))
@@ -74,17 +71,18 @@ def update_contact(id):
 @app.route('/delete/<string:id>')
 def delete_contact(id):
     cur = mysql.connection.cursor()
-    cur.execute('DELETE FROM user where id= {0};'.format(id))
+    cur.execute('DELETE FROM login where id_l= {0};'.format(id))
     mysql.connection.commit()
     flash('delete with succesfull')
     return redirect(url_for('Index'))
 data = 'jabon'
+
 #@app.route('/encript')
-def encryptar(key, data):
-    cipher = AES.new(key, AES.MODE_EAX)
-    ciphertext, tag = cipher.encrypt_and_digest(data)
-    print(data)
-    return cipher.nonce + tag + ciphertext
+# def encryptar(key, data):
+#     cipher = AES.new(key, AES.MODE_EAX)
+#     ciphertext, tag = cipher.encrypt_and_digest(data)
+#     print(data)
+#     return cipher.nonce + tag + ciphertext
 #codigo para iniciar el servidor #debug reinicia cada ves que entramos a la pag web    
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
